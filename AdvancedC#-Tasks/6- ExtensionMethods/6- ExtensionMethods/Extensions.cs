6f18a667c42c6719d5cd8c7336aecbf48f7befa5
Fatma Mahmoud <98098901+FatmaMahmoudR@users.noreply.github.com>
2025-04-16 00:25:40 +0200

    extention methods

---
+++ AdvancedC#-Tasks/6- ExtensionMethods/6- ExtensionMethods/Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _6__ExtensionMethods
+{
+    static class Extensions
+    {
+        public static int GetAge(this DateTime birthDate)
+        {
+            int age = DateTime.Now.Year - birthDate.Year;
+            return age;
+        }
+
+
+        public static double AverageEvenNumbers(this List<int> numbers)
+        {
+            double res=0;
+            int cnt = 0;
+            
+            foreach(int it in numbers){
+                if (it % 2 == 0)
+                {
+                    res += it;
+                    cnt++;
+                }
+            }
+
+            res /= cnt;
+            return res;
+        }
+
+        public static IEnumerable<T> Paginate<T>(this IEnumerable<T> source, int page, int pageSize)
+        {
+
+            int start = (page - 1) * pageSize,end = start + pageSize;
+            int i = 0;
+            foreach (var it in source)
+            {
+                if (i >= start && i < end)
+                    yield return it;
+                if (i >= end)
+                    yield break;
+                i++;
+            }
+        }
+
+
+        public static string ToJson<T>(this T obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+
+    }
+}
---
+++ AdvancedC#-Tasks/6- ExtensionMethods/6- ExtensionMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6__ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime BD = new DateTime(2008,02,29);
+            Console.WriteLine(BD.GetAge());
+
+
+            Console.WriteLine($"------------------");
+
+
+            List<int> list = new List<int>(){1, 2, 3, 4, 5, 6, 7, 8, 9};
+            Console.WriteLine(list.AverageEvenNumbers());
+
+
+            Console.WriteLine($"------------------");
+
+            var numbers = new List<int>();
+            for (int i = 1; i <= 160; i++)
+                numbers.Add(i);
+
+            foreach (var number in numbers.Paginate(2,10))
+                Console.WriteLine(number);
+            
+
+            Console.WriteLine($"------------------");
+
+
+            var e = new {ID=10, Name="Emp1" };
+
+            Console.WriteLine(e.ToJson());
+        }
+    }
+}
